489aa5c972f8e8cb66acbfa278ca0e920cc0b7fa
Maiko de Andrade <mandrade1@stefanini.com>
2023-11-10 05:26:45 +0000

    Experimento 1

---
+++ scripts/resetcore.ps1
@@ -0,0 +1,36 @@
+
+
+kubectl scale --replicas=0 deployment open5gs-ueransim01
+kubectl scale --replicas=0 deployment open5gs-ueransim02
+kubectl scale --replicas=0 deployment open5gs-ueransim03
+kubectl scale --replicas=0 deployment open5gs-ueransim04
+kubectl scale --replicas=0 deployment open5gs-ueransim05
+kubectl scale --replicas=0 deployment open5gs-ueransim06
+kubectl scale --replicas=0 deployment open5gs-ueransim07
+kubectl scale --replicas=0 deployment open5gs-ueransim08
+
+
+kubectl scale --replicas=0 deployment open5gs-upf-1
+kubectl scale --replicas=0 deployment open5gs-upf-2
+kubectl scale --replicas=0 deployment open5gs-upf-3
+kubectl scale --replicas=0 deployment open5gs-upf-4
+kubectl scale --replicas=0 deployment open5gs-upf-5
+kubectl scale --replicas=0 deployment open5gs-upf-6
+kubectl scale --replicas=0 deployment open5gs-upf-7
+kubectl scale --replicas=0 deployment open5gs-upf-8
+
+
+
+kubectl scale --replicas=0 deployment open5gs-amf
+kubectl scale --replicas=0 deployment open5gs-ausf
+kubectl scale --replicas=0 deployment open5gs-bsf
+kubectl scale --replicas=0 deployment open5gs-nrf
+kubectl scale --replicas=0 deployment open5gs-nssf
+kubectl scale --replicas=0 deployment open5gs-pcf
+kubectl scale --replicas=0 deployment open5gs-scp
+kubectl scale --replicas=0 deployment open5gs-smf
+kubectl scale --replicas=0 deployment open5gs-udm
+kubectl scale --replicas=0 deployment open5gs-udr
+kubectl scale --replicas=0 deployment open5gs-smf
+
+
---
+++ scripts/stopues.ps1
@@ -0,0 +1,12 @@
+
+
+
+
+kubectl scale --replicas=0 deployment open5gs-ue01
+kubectl scale --replicas=0 deployment open5gs-ue02
+kubectl scale --replicas=0 deployment open5gs-ue03
+kubectl scale --replicas=0 deployment open5gs-ue04
+kubectl scale --replicas=0 deployment open5gs-ue05
+kubectl scale --replicas=0 deployment open5gs-ue06
+kubectl scale --replicas=0 deployment open5gs-ue07
+kubectl scale --replicas=0 deployment open5gs-ue08
