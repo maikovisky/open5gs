7d5c498cbd7def564e92258e3db4021d416e585f
Maiko de Andrade <maikovisky@gmail.com>
2024-01-06 05:36:35 +0000

    Experimento 01

--- scripts/expslice01.sh
+++ scripts/expslice01.sh
@@ -31,49 +31,49 @@ changeLimits() {
 }
 
 
-
 start() {
     experiment="$1"
     #upf4=$(kubectl get pod | grep open5gs-upf-4 | awk '{print $1}')
     #kubectl annotate pod $upf4 kubernetes.io/egress-bandwidth-
     annotation "$experiment"
-    sleep 60s
+    sleep 30s
 
     kubectl scale --replicas=4 deployment open5gs-ue01
-    for num in 1 5 10 15 20   
+    for num in 1 5 10 15 20 
     do
        echo "Fase: $num"
        annotation "Fase: $num"
        #kubectl scale --replicas=$num deployment open5gs-ue02
        #kubectl scale --replicas=$num deployment open5gs-ue03
        #kubectl scale --replicas=$num deployment open5gs-ue04
-       sleep 495s 
+       sleep 300s 
     done 
 
+    sleep 60s 
     kubectl scale --replicas=0 deployment open5gs-ue01
     annotation "Fim do experimento"
 }
 
 
-changeLimits 1 "0" "0"
-changeLimits 2 "0" "0"
-changeLimits 3 "0" "0"
-changeLimits 4 "0" "0"
-
-start "Experimento 01 - Slice,CPU,MEM: [UPF1,nd,nd] [UPF2,nd,nd] [UPF3,nd,nd] [UPF4,nd,nd]"
-
-changeLimits 1 "1200m" "250Mi"
-changeLimits 2 "1200m" "250Mi"
-changeLimits 3 "1200m" "250Mi"
+#changeLimits 1 "0" "0"
+#changeLimits 2 "0" "0"
+#changeLimits 3 "0" "0"
 #changeLimits 4 "0" "0"
 
-start "Experimento 02 - Slice,CPU,MEM: [UPF1,1200,250] [UPF2,1200,250] [UPF3,1200,250] [UPF4,nd,nd]"
+start "Baseline 01 - Slice,CPU,MEM: [UPF1,nd,nd] [UPF2,nd,nd] [UPF3,nd,nd] [UPF4,nd,nd]"
 
 #changeLimits 1 "1200m" "250Mi"
-changeLimits 2 "0" "0"
+#changeLimits 2 "1200m" "250Mi"
 #changeLimits 3 "1200m" "250Mi"
-changeLimits 4 "1200m" "250Mi"
+#changeLimits 4 "0" "0"
 
-start "Experimento 03 - Slice,CPU,MEM: [UPF1,1200,250] [UPF2,nd,nd] [UPF3,1200,250] [UPF4,1200,1200]"
+#start "Experimento 02 - Slice,CPU,MEM: [UPF1,1200,250] [UPF2,1200,250] [UPF3,1200,250] [UPF4,nd,nd]"
+
+#changeLimits 1 "1200m" "250Mi"
+#changeLimits 2 "0" "0"
+#changeLimits 3 "1200m" "250Mi"
+#changeLimits 4 "1200m" "250Mi"
+
+#start "Experimento 03 - Slice,CPU,MEM: [UPF1,1200,250] [UPF2,nd,nd] [UPF3,1200,250] [UPF4,1200,1200]"
 
 
